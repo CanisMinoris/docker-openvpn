e8cdbfa233fc0d29df64e8020caf36eb23f26bb2
Stanisalv Rossovskiy <s.rossovskiy@zaptain.dev>
2024-10-22 16:37:23 +0400

    fix: correct server certificate generation using EASYRSA_REQ_CN for proper CN setting

--- files/configuration/setup_pki.sh
+++ files/configuration/setup_pki.sh
@@ -1,21 +1,14 @@
-if [ ! -f "$PKI_DIR/issued/$OVPN_SERVER_CN.crt" ] || [ "$REGENERATE_CERTS" == 'true' ]; then
-
+if [ ! -f "$PKI_DIR/issued/server.crt" ] || [ "$REGENERATE_CERTS" == 'true' ]; then
  echo "easyrsa: creating server certs"
  sed -i 's/^RANDFILE/#RANDFILE/g' /opt/easyrsa/openssl-easyrsa.cnf
  EASYCMD="/opt/easyrsa/easyrsa"
  . /opt/easyrsa/pki_vars
+ 
+ export EASYRSA_REQ_CN="$OVPN_SERVER_CN"
+ 
  $EASYCMD init-pki
-
  $EASYCMD build-ca nopass
-
  $EASYCMD gen-dh
  openvpn --genkey secret $PKI_DIR/ta.key
-
- $EASYCMD build-server-full "$OVPN_SERVER_CN" nopass
-
-# if [ "${USE_CLIENT_CERTIFICATE}" == "true" ] ; then
-#  echo "easyrsa: creating client certs"
-#  $EASYCMD build-client-full client nopass
-# fi
-
+ $EASYCMD build-server-full server nopass
 fi
